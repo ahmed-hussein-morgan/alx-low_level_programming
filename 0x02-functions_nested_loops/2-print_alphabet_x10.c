7706433493eb425e6878cc98765ebd21c6d2fa32
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-07-18 20:39:30 +0200

    update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print_alphabet 10 times
+ *
+ * Return: alphabet
+*/
+void print_alphabet_x10(void)
+{
+	int i, n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+	_putchar('\n');
+	}
+}
