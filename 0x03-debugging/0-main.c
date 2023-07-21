5ad1ef032244ca10c265ce5165cecf29aa55f85f
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-07-21 14:38:15 +0200

    update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,8 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
@@ -13,17 +10,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	else
-	{
+	if(n == 0)
 		printf("%d is zero\n", n);
-	}
 	return (0);
 }
