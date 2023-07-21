a4d44f55191fd13dcb9c2a176795bc694362ff62
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-07-21 04:02:58 +0200

    update

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - returns the largest of 3 numbers
+ * @c: third integer
+ * Return: largest number
+*/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
