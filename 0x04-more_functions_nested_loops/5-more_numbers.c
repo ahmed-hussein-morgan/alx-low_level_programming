c88761e5e41d4e951567de6b3f4b65e6c9d6e031
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-07-22 05:30:31 +0200

    update

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+*/
+void more_numbers(void)
+{
+	int i, j, k;
+
+	i = '0';
+	j = '0';
+	k = 0;
+
+	while(k < 10)
+	{
+		for (; i <= '9'; i++)
+		{
+			_putchar(i);
+		}
+		i = '1';
+		for (; j <= '4'; j++)
+		{
+			_putchar(i);
+			_putchar(j);
+		}
+		_putchar('\n');
+		k++;
+	}
+}
