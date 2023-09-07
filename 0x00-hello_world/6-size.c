b4c279c393a1875a471590dcf0f513d07a204236
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2023-09-07 12:56:59 -0700

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,5 +9,8 @@ int main(void)
 {
 	printf("Size of a char: %c byte(s)", char);
 	printf("Size of an int: %i byte(s)", int);
+	printf("Size of a long int: %l byte(s)", long int);
+	printf("Size of a long long int: %ll byte(s)", long long int);
+	printf("Size of a float: %d byte(s)", int);
 	return (0);
 }
