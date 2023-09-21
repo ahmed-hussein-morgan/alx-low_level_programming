eaa2c16251883e9a60395056860cd96923eab158
root <root@Ahmed-M>
2023-09-21 03:01:34 +0300

    update

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: conc str
+ * @src: appended str
+ * @n: number of needed bytes
+ * Return: concatenated str
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (;j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	return (dest);
+}
