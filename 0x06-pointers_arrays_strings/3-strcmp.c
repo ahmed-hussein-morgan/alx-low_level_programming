b65253bea36b74d23a209db2a5fb4224bcada0b8
root <root@Ahmed-M>
2023-09-22 05:13:04 +0300

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,11 +13,11 @@ int _strcmp(char *s1, char *s2)
 	{
 		return (3);
 	}
-	for ( ;s1[i] != '\0' || s2[i] != '\0'; i++)
+	for ( ; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] == s2[i])
 		{
-			return(0);
+			return (0);
 		}
 		else if (s1[i] < s2[i])
 		{
@@ -26,5 +26,5 @@ int _strcmp(char *s1, char *s2)
 		else
 			return (15);
 	}
-	return(0);
+	return (0);
 }
