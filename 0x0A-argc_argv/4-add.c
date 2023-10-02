db7edb8d29505e4a73ef9b33dea6e705565f82b4
root <root@Ahmed-M>
2023-10-02 22:49:25 +0300

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,21 +6,17 @@
  * @argv: the array of arguments
  * Return: Always 0 (Success)
 */
-int check_chr(char *c);
 int main(int argc, char *argv[])
 {
 	int i, sum = 0;
-	/*char *c;*/
-	/*char *arg;*/
 
 	if (argc == 1)
 	{
 		printf("%c\n", '0');
 	}
-	for(i = 0; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
-		/*arg  = argv[i];*/
-		if(atoi(argv[i]) < 0)
+		if (atoi(argv[i]) < 0)
 		{
 			printf("Error\n");
 			return (1);
@@ -30,14 +26,3 @@ int main(int argc, char *argv[])
 	printf("%i\n", sum);
 	return (0);
 }
-/**
-int check_chr(char *c)
-{
-	int x;
-	if (atoi(c))
-	{
-		x = atoi(c);
-		return (x);
-	}
-	return (0);
-}*/
