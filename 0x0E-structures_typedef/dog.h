852f8285f920b2cc68a25ddcf312f164150db17f
root <root@Ahmed-M>
2023-10-15 09:23:37 +0300

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-struct dog {
+/**
+ * struct dog - a dog struct
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner name of the dog
+ *
+ * Description - this is a dog id
+*/
+struct dog
+{
 
 	char *name;
 	float age;
