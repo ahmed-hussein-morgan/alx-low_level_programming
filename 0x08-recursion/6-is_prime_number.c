14757216746a305e465338bda21168bb57813e24
root <root@Ahmed-M>
2023-10-26 06:12:21 +0300

    update the header file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,28 +1,4 @@
 #include "main.h"
-#include <math.h>
-
-/**
- * check_divisibility - check if the number n is divisible by any number up to i
- * @n: the number
- * @i: the current divisor
- * Return: 0 if n is divisible by any number up to i, 1 otherwise
-*/
-int check_divisibility(int n, int i)
-{
-    if (i > sqrt(n))
-    {
-        return 1;
-    }
-    else if (n % i == 0 || n % (i + 2) == 0)
-    {
-        return 0;
-    }
-    else
-    {
-        return check_divisibility(n, i + 6);
-    }
-}
-
 /**
  * is_prime_number - check if the number is prime
  * @n: the number
@@ -30,20 +6,5 @@ int check_divisibility(int n, int i)
 */
 int is_prime_number(int n)
 {
-    if (n <= 1)
-    {
-        return 0;
-    }
-    else if (n <= 3)
-    {
-        return 1;
-    }
-    else if (n % 2 == 0 || n % 3 == 0)
-    {
-        return 0;
-    }
-    else
-    {
-        return check_divisibility(n, 5);
-    }
+
 }
