f9979c5699834d849f1c563747db5de18f3a70cf
root <root@Ahmed-M>
2023-10-27 09:48:30 +0200

    add betty documentation for created functions

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * is_prime_number - check if the number is prime
+ * is_prime_helper - analize the number in details
  * @n: the number
- * @i: the current divisor being checked
- * Return: the check result
+ * @i: the half of the intger
+ * Return: the result of the number anlysis
  */
+
 int is_prime_helper(int n, int i)
 {
 	if (i == 1)
@@ -25,6 +26,11 @@ int is_prime_helper(int n, int i)
 	}
 }
 
+/**
+ * is_prime_number - confirm the prime number
+ * @n: the number
+ * Return: the check result
+ */
 int is_prime_number(int n)
 {
 	if (n <= 1)
