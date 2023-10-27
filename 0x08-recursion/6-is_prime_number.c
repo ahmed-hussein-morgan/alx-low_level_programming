1531466026474a70ad48936c53b187e6e6c1cd3d
root <root@Ahmed-M>
2023-10-27 05:14:19 +0200

    betty correction

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,32 +8,31 @@
  */
 int is_prime_helper(int n, int i)
 {
-    if (i == 1)
-    {
-        return 1;
-    }
-    else
-    {
-        if (n % i == 0)
-        {
-            return 0;
-        }
-        else
-        {
-            return is_prime_helper(n, i - 1);
-        }
-    }
+	if (i == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (is_prime_helper(n, i - 1));
+		}
+	}
 }
 
 int is_prime_number(int n)
 {
-    if (n <= 1)
-    {
-        return 0;
-    }
-    else
-    {
-        return is_prime_helper(n, n / 2);
-    }
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (is_prime_helper(n, n / 2));
+	}
 }
-
