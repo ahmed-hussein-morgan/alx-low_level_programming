df37bcbb9434b362eec4769346bd9c084d48d7d0
root <root@Ahmed-M>
2023-11-18 03:31:04 +0200

    update

---
+++ 0x12-singly_linked_lists/3-add_node_end-my.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: the head pointer of the list
+ * @str: the string
+ * Return: the address of the new element, or NULL if it failed
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+
+}
