1ef2b1d8fefe8318ccf9d7778d0b8af9a7efd454
root <root@Ahmed-M>
2023-11-19 04:35:43 +0200

    100-first.c => try to fix betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
-void before_main() __attribute__((constructor));
-void before_main() {
-   printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+/**
+ * before_main - printing before the main() is excecuted
+ *
+ * Return: NO Return
+*/
+
+void before_main(void) __attribute__((constructor));
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n")
+	printf("I bore my house upon my back!\n");
 }
