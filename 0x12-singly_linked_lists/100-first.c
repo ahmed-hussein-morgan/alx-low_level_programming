b88ba3213fec55f8e64fbd0c70cb41656dc068f0
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2023-11-18 18:24:39 -0800

    100-first.c => printing before the main function is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,6 @@
-#include <stdio.h>
-/**
- * before_main - print string before main func exec
-*/
-void before_main(void) __attribute__((constructor));
-void before_main(void)
-{
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+#include<stdio.h>
+
+void before_main() __attribute__((constructor));
+void before_main() {
+   printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
