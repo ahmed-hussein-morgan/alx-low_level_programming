10828ea0b5313230766de02c89e738ab5a84bc91
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2023-12-08 20:49:14 -0800

    fix betty and increase the buffer size

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,29 +1,34 @@
 #include "main.h"
 
-
+/**
+ * read_textfile -  reads a text file and prints it to standard output.
+ * @filename: the source file to read.
+ * @letters: number of letters to read.
+ * Return: the actual number of letters it could read and print
+*/
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, w, r;
-	char buffer[1024];
+	char buffer[2048];
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
 
-	fd = open(filename, O_RDWR);
+	fd = open(filename, O_RDONLY | O_WRONLY);
 	if (!fd)
 	{
 		return (0);
 	}
 	r = read(fd, buffer, letters);
-	if (!r)
+	if (r == -1)
 	{
 		return (0);
 	}
 	w = write(STDOUT_FILENO, buffer, r);
-	if (!w)
+	if (w == -1)
 	{
 		return (0);
 	}
